dd1dc409735bec5b1e62ef025da05d746df990ba
Silas Gyger <silasgyger@gmail.com>
2021-08-14 20:42:20 +0200

    added ability to view markdown filse

--- src/views/Files.js
+++ src/views/Files.js
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab'
+import {MdFile} from '../components/MarkdownDisplay/MarkdownDisplay'
+import _ from 'lodash'
 
 // react-bootstrap components
 import {
@@ -8,9 +10,25 @@ import {
   Container,
   Row,
   Col,
+  ListGroup,
+  Badge
 } from "react-bootstrap";
 
 function Files() {
+
+  const [selectedKey, setSelectedKey] = useState("econs");
+
+  const files = {
+    econs: {
+      url: "https://ipfs.io/ipfs/QmSwgRLQ5hu8EmgG1wF3ghU2jDZKsPcmnUX7VgUecru2Fx?filename=test.md",
+      title: "Intro to Econs"
+    },
+
+    lnotes: {
+      url: "http://bafybeicea47cjq4jotade5aq2knav3ocwlxsi2nzi2cmypsrfqbcz4xlum.ipfs.localhost:8080/",
+      title: "Lecture Notes"
+    }
+  }
 
   return (
     <>
@@ -18,45 +36,29 @@ function Files() {
         <Row>
           <Col md="12">
             <Card>
-              <Card.Header>
-              <Tabs defaultActiveKey="econs" id="uncontrolled-tab-example" className="mb-3">
-
-  <Tab eventKey="econs" title="Economics" onSelect>
-  <Card.Title as="h4">Download Your Files Here</Card.Title>
-  <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                <Card.Title as="h4">
-                <a href="http://bafybeicea47cjq4jotade5aq2knav3ocwlxsi2nzi2cmypsrfqbcz4xlum.ipfs.localhost:8080/" download target="_blank">
-                  Intro to Econs.pdf
-                </a>
-                </Card.Title>
-                <Card.Title as="h4">
-                <a href="http://bafybeicea47cjq4jotade5aq2knav3ocwlxsi2nzi2cmypsrfqbcz4xlum.ipfs.localhost:8080/" download target="_blank">
-                  Lecture Notes 2
-                </a>
-                </Card.Title>
-                </div>
-              </Card.Footer>
-  </Tab>
-  <Tab eventKey="chem" title="Chemistry">
-  <Card.Title as="h4">Download Your Files Here</Card.Title>
-  <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                <Card.Title as="h4">
-                <a href="http://bafybeicea47cjq4jotade5aq2knav3ocwlxsi2nzi2cmypsrfqbcz4xlum.ipfs.localhost:8080/" download target="_blank">
-                  lecture1a.ppt
-                </a>
-                </Card.Title>
-                </div>
-              </Card.Footer>
-  </Tab>
-</Tabs>
-
-
-              </Card.Header>
-
+              <Card.Header> View/Download your files here: </Card.Header>
+              <Card.Body>
+                <ListGroup>
+                  {
+                    _.map(_.toPairs(files), ([key, file]) => <ListGroup.Item
+                                                                key={key}
+                                                                active={selectedKey==key}
+                                                                action
+                                                                onClick={() => setSelectedKey(key)}
+                                                                >
+                                                                  {
+                                                                  file.url.endsWith("md") ? <Badge bg="info">MD</Badge> : <></>
+                                                                  }
+                                                                  {file.title}
+                                                              </ListGroup.Item>)
+                  }
+                </ListGroup>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Body>
+                  <MdFile url={files[selectedKey]?.url} />
+              </Card.Body>
             </Card>
           </Col>
         </Row>
